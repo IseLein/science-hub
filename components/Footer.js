66f8963a620a99452f803c0d11489f5411c1ee4c
Daniel Iseoluwa <87953026+IseLein@users.noreply.github.com>
2022-10-16 02:52:45 -0400

    Worked on some errors and .eslintrc

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className={styles.footer}>
             Science Hub &copy; 2022.{"  "}
-            <a target="_blank" href="https://icons8.com/icon/_m9zhw01DmxX/bbb">Bbb</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+            <a href="https://icons8.com/icon/_m9zhw01DmxX/bbb">Bbb</a> icon by <a href="https://icons8.com">Icons8</a>
         </div>
     )
 };
