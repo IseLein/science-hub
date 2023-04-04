fa352119695b73e9bf82cd3fcb0c40eb68845a1c
Daniel Iseoluwa <87953026+IseLein@users.noreply.github.com>
2023-04-04 04:26:59 -0400

    added page analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { SessionProvider } from "next-auth/react"
 import { ThemeProvider } from "next-themes"
+import { Analytics } from "@vercel/analytics/react"
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
@@ -7,6 +8,7 @@ function MyApp({ Component, pageProps }) {
     <SessionProvider session={pageProps.session}>
       <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
       </ThemeProvider>
     </SessionProvider>
   )
