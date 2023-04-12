5fd0552965b02463adab3420e686978c03880884
Daniel Iseoluwa <87953026+IseLein@users.noreply.github.com>
2023-04-12 04:25:35 -0400

    added model and api route for category

---
+++ models/Category.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const Category = new mongoose.Schema({
+    category: {
+        type: String,
+        required: true,
+    },
+    bannerImage: {
+        type: String,
+        required: true,
+    },
+    posts: {
+        type: [String],
+        required: true,
+    },
+})
+
+export default mongoose.models.Category || mongoose.model("Category", Category)
--- pages/api/article/updateCategory.js
+++ pages/api/article/updateCategory.js
@@ -1,23 +1,36 @@
-import dbConnect from '../../../util/dbConnect';
-import Blog from '../../../models/Blog';
+import dbConnect from "../../../util/dbConnect";
+import Category from "../../../models/Category";
 import { getToken } from 'next-auth/jwt';
 
 /**
  * @param {import('next').NextApiRequest} req
  * @param {import('next').NextApiResponse} res
  */
-export default async function addArticle(req, res){
+export default async function updateCategory(req, res) {
     const token = await getToken({ req });
     if (!token) {
         res.status(401).json({ success:false, error: "uhh, you're not allowed to do that" });
     }
 
     try {
-        await dbConnect(); // Connect to the Database
-        const article = await Blog.create(req.body); // Create and add the Blog 'object'
-        res.status(201).json({ success: true, article });
+        await dbConnect();
+        let updatedData = [];
+
+        const categories = req.body.categories;
+        for (let category of categories) {
+            let data = await Category.findOne({ category: category });
+
+            let newPosts = data.posts;
+            newPosts[newPosts.length] = req.body.post_id;
+
+            let update = await Category.findByIdAndUpdate(data._id,
+                { posts: newPosts }, { new: true }
+            );
+            updatedData[updatedData.length] = update;
+        }
+        res.status(201).json({ success: true, updates: updatedData });
     } catch(error) {
-        let errorMessage = "Unable to create blog post.";
+        let errorMessage = "Unable to create category";
         let statusCode = 500;
 
         if (error.name === "ValidationError") {
