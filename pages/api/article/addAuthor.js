cf2608e3117172bf1a26a7e7a49bf9fcefac7526
Daniel Iseoluwa <87953026+IseLein@users.noreply.github.com>
2023-04-13 06:49:01 -0400

    added descriptive error messages on author api

--- pages/api/article/addAuthor.js
+++ pages/api/article/addAuthor.js
@@ -15,8 +15,21 @@ export default async function addAuthor(req, res){
     try {
         await dbConnect(); // Connect to the Database
         const author = await Author.create(req.body); // Create and add the Blog 'object'
-        res.json({ author });
+        res.status(201).json({ success: true, author });
     } catch(error) {
-        res.json({ error })
+        let errorMessage = "Unable to create record for contributor.";
+        let statusCode = 500;
+
+        if (error.name === "ValidationError") {
+            errorMessage = "Invalid input. Please provide all required fields.";
+            statusCode = 400;
+        } else if (error.code === 11000) {
+            errorMessage = "Duplicate key error. Please provide a unique value for the field.";
+            statusCode = 400;
+        } else if (error.name === "MongoError") {
+            errorMessage = "Database Error. Please try again later.";
+            statusCode = 400;
+        }
+        res.status(statusCode).json({ success: false, error: errorMessage });
     }
 }
