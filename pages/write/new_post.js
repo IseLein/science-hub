7d0fc1c35badecfa58451e54cb8f3b03470d0895
iselein <iseoluwagbotemidaniel@gmail.com>
2023-05-12 13:05:29 -0400

    added editor (no logic or added ui)

--- pages/write/new_post.js
+++ pages/write/new_post.js
@@ -3,7 +3,10 @@ import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import Navbar from "../../components/Navbar";
 import SignInOut from "../../components/SignInOut";
-import styles from "../../styles/Write.module.css";
+import Footer from "../../components/Footer";
+import { useEffect, useState } from "react";
+import { Editor } from "@toast-ui/react-editor";
+import '@toast-ui/editor/dist/toastui-editor.css';
 
 export default function NewPost() {
     const { data: session } = useSession();
@@ -37,13 +40,19 @@ export default function NewPost() {
 
         const response = await fetch(endpoint, options);
         const result = await response.json();
-    
+
         if (response.ok) {
             alert("New Post added successfully");
             router.push("/write/");
         } else {
             alert(result.error);
         }
+    }
+
+    const [content, setContent] = useState("flowzzzz");
+    let updateContent = () => {
+        let contentEl = document.getElementById("content")
+        contentEl.textContent = content
     }
 
     return(
@@ -55,46 +64,28 @@ export default function NewPost() {
                 name: "New Post",
                 link: "",
             }}/>
-            <div className={styles.article_hero}></div>
+            <div className="pt-28"></div>
             {session &&
-                <div className={styles.new_blog}>
-                <h1>{session.user.name}</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className={styles.input_block}>
-                        <label htmlFor="title" className={styles.input_label}>Title</label>
-                        <input required type="text" name="title" id="title" className={styles.input_box}></input>
+                <div>
+                    <div className="px-10 md:px-28 lg:px-48 lg:text-xl">
+                        <Editor
+                            initialValue={content}
+                            previewStyle="tab"
+                            height="600px"
+                            initialEditType="wysiwyg"
+                            useCommandShortcut={true}
+                            theme="dark"
+                            onChange={(e) => {
+                                setContent(e.target.value);
+                                updateContent();
+                            }}
+                        />
+                        <div id="content"></div>
                     </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="author" className={styles.input_label}>Author</label>
-                        <input required type="text" name="author" id="author" className={styles.input_box} value={session.user.name} readOnly></input>
-                    </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="category" className={styles.input_label}>Category</label>
-                        <input required type="text" name="category" id="category" className={styles.input_box}></input>
-                    </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="src" className={styles.input_label}>Src</label>
-                        <input required type="text" name="src" id="src" className={styles.input_box}></input>
-                    </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="alt" className={styles.input_label}>Alt</label>
-                        <input required type="text" name="alt" id="alt" className={styles.input_box}></input>
-                    </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="description" className={styles.input_label}>Description</label>
-                        <input required type="text" name="description" id="description" className={styles.input_box}></input>
-                    </div>
-                    <div className={styles.input_block}>
-                        <label htmlFor="content" className={styles.input_label}>Content</label>
-                        <textarea required rows={25} type="text" name="content" id="content" className={styles.input_box_ta}></textarea>
-                    </div>
-                    <div className={styles.buttons}>
-                        <button className={styles.btn} type="submit">SAVE</button>
-                    </div>
-                </form>
-            </div>
+                </div>
             }
             {!session && <SignInOut />}
+            <Footer />
         </div>
     )
 };
