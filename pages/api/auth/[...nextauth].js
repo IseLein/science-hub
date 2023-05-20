ae8eb7d5ad6f68a17031f4459221f6b0081da007
IseLein <iseoluwagbotemidaniel@gmail.com>
2023-05-19 23:45:44 -0400

    removed Github Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth/next";
-import GitHubProvider from "next-auth/providers/github";
+// import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 const options = {
@@ -8,11 +8,11 @@ const options = {
             clientId: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         }),
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-            authorization: { params: { scope: "user" } }
-        }),
+        // GitHubProvider({
+        //     clientId: process.env.GITHUB_ID,
+        //     clientSecret: process.env.GITHUB_SECRET,
+        //     authorization: { params: { scope: "user" } }
+        // }),
     ],
     session: {
         jwt: true,
