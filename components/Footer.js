c1456039350586c3bc5e7bcf38634eebf56b6932
IseLein <iseoluwagbotemidaniel@gmail.com>
2023-05-20 00:13:31 -0400

    footer 2023

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <div className="mt-44 text-center text-amber-900 dark:text-orange-300 font-semibold">
-            Science Hub &copy; 2022.{"  "}
+            Science Hub &copy; 2023.{"  "}
             <a href="https://icons8.com/icon/_m9zhw01DmxX/bbb" target="_blank" rel="noopener noreferrer">
                 Bbb
             </a> icon by 
